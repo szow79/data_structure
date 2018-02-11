fa11bde00940227ccbd146445a6e2d88c9e96776
szow79 <szow79@gmail.com>
2018-02-11 10:58:28 -0800

    Hashing

--- week1/lib/p02_hashing.rb
+++ week1/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    h = 0
+    self.each_with_index do |c, i|
+      h =+ c.to_s.ord * i
+    end
+    h
   end
 end
 
 class String
   def hash
+    h = 0
+    self.chars.each_with_index do |c, i|
+      h =+ c.ord * i
+    end
+    h
   end
 end
 
@@ -16,6 +26,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.flatten.inject(0) do |sum, arr|
+      sum + arr.hash
+    end
   end
 end

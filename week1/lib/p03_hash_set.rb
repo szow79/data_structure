e9a24475cd5f6d5e29166c3d929cba6ae6fa517b
szow79 <szow79@gmail.com>
2018-02-11 11:13:25 -0800

    Hashset

--- week1/lib/p03_hash_set.rb
+++ week1/lib/p03_hash_set.rb
@@ -9,18 +9,26 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      self[key.hash] << key
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +36,13 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each do |n|
+        insert(n)
+      end
+    end
   end
 end

d2838554f9e62e9f343e732af36c9b51af227a30
szow79 <szow79@gmail.com>
2018-02-25 17:23:03 -0800

    temp

--- week2/bst.rb
+++ week2/bst.rb
@@ -1,89 +1,42 @@
 class Node
-  attr_reader :val
+  attr_reader :key
   attr_accessor :left, :right
 
-  def initialize(val)
-    @val = val
+  def initialize(key)
+    @key = key
     @left = nil
     @right = nil
   end
-end
 
-class BinaryTree
-  attr_reader :root
-
-  def initialize
-    @root = nil
-  end
-
-  def add(val)
-    if @root.nil?
-      @root = Node.new(val)
+  def insert(key)
+    if self.key > key
+      if self.left
+        self.left.insert(key)
+      else
+        self.left = Node.new(key)
+      end
     else
-      current_node = @root
-
-      loop do
-        if current_node.val == val
-          return "Node already exists!"
-        elsif current_node.val > val
-          if current_node.left.nil?
-              current_node.left = Node.new(val)
-            break
-          end
-          current_node = current_node.left
-        else
-          if current_node.right.nil?
-              current_node.right = Node.new(val)
-            break
-          end
-          current_node = current_node.right
-        end
-      end
-    end
-  end
-
-  def delete(val)
-    @root = nil if @root && @root.val == val
-    current_node = @root
-    loop do
-      if current_node.val > val
-        if current_node.left.val == val
-          current_node.left = nil
-          break
-        elsif
-          current_node.left.nil?
-          break
-        end
-        current_node = current_node.left
+      if self.right
+        self.right.insert(key)
       else
-        if current_node.right.val == val
-          current_node.right = nil
-          break
-        elsif
-          current_node.right.nil?
-          break
-        end
-        current_node = current_node.right
+        self.right = Node.new(key)
       end
     end
   end
 
-  def find(val)
-    return unless @root
-    current_node = @root
-    while current_node
-      if current_node.val == val
-        return current_node
-      elsif current_node.val > val
-        current_node = current_node.left
-      else
-        current_node = current_node.right
-      end
+  def lookup(key)
+    return false unless self
+    return self if self.key == key
+    if self.key > key
+      self.left.loopup(key)
+    else
+      self.right.loopup(key)
     end
-    current_node
   end
 
-  def exist?(val)
-    !find(val).nil?
+  def delete(key)
+
   end
+
 end
+

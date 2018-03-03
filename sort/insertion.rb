d757ede33ba50878404c076ef61a321d04c51dc6
szow79 <szow79@gmail.com>
2018-03-03 10:42:33 -0800

    insertion sort

--- sort/insertion.rb
+++ sort/insertion.rb
@@ -1,3 +1,12 @@
 def insertion_sort(arr)
-
+  i = 1
+  while i < arr.length
+    j = i
+    while j > 0 && arr[j] < arr[j-1]
+      arr[j], arr[j-1] = arr[j-1], arr[j]
+      j -= 1
+    end
+    i += 1
+  end
+  arr
 end

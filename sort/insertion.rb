b5d9c518503d497db0d78cc71db83662b49c0b21
szow79 <szow79@gmail.com>
2018-03-03 10:37:54 -0800

    move insertion sort code to selection sort

--- sort/insertion.rb
+++ sort/insertion.rb
@@ -1,16 +1,3 @@
 def insertion_sort(arr)
-  i = 0
-  while i < arr.length
-    j = i
-    current_min = {index: nil, value: nil}
-    while j < arr.length
-      if current_min[:value].nil? || current_min[:value] > arr[j]
-        current_min = {index: j, value: arr[j]}
-      end
-      j += 1
-    end
-    arr[i], arr[current_min[:index]] = arr[current_min[:index]], arr[i]
-    i += 1
-  end
-  arr
+
 end
